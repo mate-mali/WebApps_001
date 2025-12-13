72dc2cbc0d1295e668437e495171c0fb0faf946b
matemali <mateusz.malinowski.1991@gmail.com>
2025-12-13 11:21:59 +0100

    Added feature to add applications back to database using forms

--- src/flask_project.py
+++ src/flask_project.py
@@ -1,20 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
     cursor.execute('SELECT * FROM movies')
     # for row in cursor:
     #     outputx.append([row[0], row[1], row[2], row[3]])
     return render_template('home.html', movies = cursor)
-    # db.close()
+    db.close()
 
-@app.route("/addMovies")
+@app.route("/addMovies", methods=['GET', 'POST'])
 def add_movies():
-    #return ("<b>Jestem na stronie dodawania nowego filmu</b><a href='/'>Idz do strony domowej</a>")
+    if request.method == 'POST':
+        movieTitle = request.form.get('title')
+        movieYear = request.form.get('year')
+        movieActors = request.form.get('actors')
+        print(movieTitle, movieYear, movieActors)
+        db = sqlite3.connect('movies.db')
+        cursor = db.cursor()
+        cursor.execute(f'''
+                        INSERT INTO movies 
+                       (title, year, actors)
+                       values(
+                       '{movieTitle}',
+                       '{movieYear}',
+                       '{movieActors}'
+                       )
+                       ''')
+        db.commit()
+        db.close()
+        return redirect(url_for('home'))
     return render_template('add.html')
 
 
