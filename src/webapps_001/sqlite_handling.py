9b4413f308f72b2f1a0723e8667fbfa3d8d1bd22
matemali <mateusz.malinowski.1991@gmail.com>
2025-12-13 09:15:04 +0100

    added script to manage sqlite

---
+++ src/webapps_001/sqlite_handling.py
@@ -0,0 +1,24 @@
+import sqlite3
+db = sqlite3.connect('movies.db')
+cursor = db.cursor()
+
+def listMovies():
+    cursor.execute('SELECT * FROM movies')
+    for row in cursor:
+        print('{0}, {1}, {2}'.format(row[1], row[2], row[3]))
+
+def addMovie(title, year, actors):
+    cursor.execute(f'INSERT INTO movies (title, year, actors) VALUES ("{title}",{year},"{actors}")')
+    db.commit()
+
+def findMovie(key):
+    cursor.execute('SELECT * FROM movies WHERE title LIKE ? OR actors LIKE ?', ("%"+key+"%", "%"+key+"%"))
+    # cursor.execute(f'SELECT * FROM movies WHERE title LIKE "%{key}%" OR actors LIKE "%{key}%"') - niebezpieczne, bo SQLInjection
+    for row in cursor:
+        print('{0}, {1}, {2}'.format(row[1], row[2], row[3]))
+
+
+# addMovie("test movie", 1923, "actors")
+listMovies()
+# findMovie("Indi")
+
