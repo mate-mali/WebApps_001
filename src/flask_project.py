2fe9a106e4c1f93aa44328e07c881a3eead67dd6
mate-mali <mateusz.malinowski.1991@egmail.com>
2026-01-05 22:00:51 +0100

    somehow i made removing via form working in quite efficient way

--- src/flask_project.py
+++ src/flask_project.py
@@ -2,14 +2,31 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    db = sqlite3.connect('movies.db')
-    cursor = db.cursor()
-    cursor.execute('SELECT * FROM movies')
-    return render_template('home.html', movies = cursor)
-
+    if request.method == 'GET':
+        db = sqlite3.connect('movies.db')
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM movies')
+        return render_template('home.html', movies = cursor)
+   
+    
+@app.route("/deleteMovies", methods=['POST'])
+def deleteMovies():
+    if request.method == 'POST':
+        datalist = request.form.getlist('moviesToRemove') #unique keys to remove 
+        remove_ids = f"({','.join(datalist)})"
+        print(remove_ids)
+        db = sqlite3.connect('movies.db')
+        cursor = db.cursor()
+        cursor.execute(f'''
+                       DELETE FROM movies 
+                       WHERE 
+                       ID in {remove_ids}
+                       ''')
+        db.commit()
+        return redirect(url_for('home'))
+  
 @app.route("/addMovies", methods=['GET', 'POST', 'DELETE'])
 def add_movies():
     if request.method == 'POST':
@@ -29,12 +46,11 @@ def add_movies():
                        )
                        ''')
         db.commit()
-        # db.close()
         return redirect(url_for('home'))
     return render_template('add.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.debug = True
+    #app.debug = True
